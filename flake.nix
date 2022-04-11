8b24f274fbad0d6c3d8b51e39db66c845aa6eab1
Sridhar Ratnakumar <srid@srid.ca>
2022-04-11 19:29:57 -0400

    Add hercules CI setting to flake.nix

--- flake.nix
+++ flake.nix
@@ -13,58 +13,61 @@
     unionmount.inputs.nixpkgs.follows = "ema/nixpkgs";
   };
   outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        name = "ema-template";
-        overlays = [ ];
-        pkgs = nixpkgs.legacyPackages.${system};
-        hp = pkgs.haskellPackages;
-        tailwind-haskell = inputs.tailwind-haskell.defaultPackage.${system};
-        # Based on https://github.com/input-output-hk/daedalus/blob/develop/yarn2nix.nix#L58-L71
-        filter = name: type:
-          let
-            baseName = baseNameOf (toString name);
-            sansPrefix = pkgs.lib.removePrefix (toString ./.) name;
-          in
-          # Ignore these files when building source package
-            !(
-              baseName == "README.md" ||
-              sansPrefix == "/bin" ||
-              sansPrefix == "/content" ||
-              sansPrefix == "/.github" ||
-              sansPrefix == "/.vscode" ||
-              sansPrefix == "/.ghcid"
-            );
-        project = returnShellEnv:
-          hp.developPackage {
-            inherit returnShellEnv name;
-            root = pkgs.lib.cleanSourceWith { inherit filter name; src = ./.; };
-            withHoogle = false;
-            overrides = self: super: with pkgs.haskell.lib; {
-              ema = inputs.ema.defaultPackage.${system};
-              tailwind = tailwind-haskell;
-              path-tree = self.callCabal2nix "path-tree" inputs.pathtree { };
-              unionmount = self.callCabal2nix "unionmount" inputs.unionmount { };
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          name = "ema-template";
+          overlays = [ ];
+          pkgs = nixpkgs.legacyPackages.${system};
+          hp = pkgs.haskellPackages;
+          tailwind-haskell = inputs.tailwind-haskell.defaultPackage.${system};
+          # Based on https://github.com/input-output-hk/daedalus/blob/develop/yarn2nix.nix#L58-L71
+          filter = name: type:
+            let
+              baseName = baseNameOf (toString name);
+              sansPrefix = pkgs.lib.removePrefix (toString ./.) name;
+            in
+            # Ignore these files when building source package
+              !(
+                baseName == "README.md" ||
+                sansPrefix == "/bin" ||
+                sansPrefix == "/content" ||
+                sansPrefix == "/.github" ||
+                sansPrefix == "/.vscode" ||
+                sansPrefix == "/.ghcid"
+              );
+          project = returnShellEnv:
+            hp.developPackage {
+              inherit returnShellEnv name;
+              root = pkgs.lib.cleanSourceWith { inherit filter name; src = ./.; };
+              withHoogle = false;
+              overrides = self: super: with pkgs.haskell.lib; {
+                ema = inputs.ema.defaultPackage.${system};
+                tailwind = tailwind-haskell;
+                path-tree = self.callCabal2nix "path-tree" inputs.pathtree { };
+                unionmount = self.callCabal2nix "unionmount" inputs.unionmount { };
+              };
+              modifier = drv:
+                pkgs.haskell.lib.addBuildTools drv
+                  (with hp; [
+                    cabal-fmt
+                    cabal-install
+                    ghcid
+                    haskell-language-server
+                    ormolu
+                    pkgs.nixpkgs-fmt
+                    tailwind-haskell
+                    pkgs.foreman
+                  ]);
             };
-            modifier = drv:
-              pkgs.haskell.lib.addBuildTools drv
-                (with hp; [
-                  cabal-fmt
-                  cabal-install
-                  ghcid
-                  haskell-language-server
-                  ormolu
-                  pkgs.nixpkgs-fmt
-                  tailwind-haskell
-                  pkgs.foreman
-                ]);
-          };
-      in
-      {
-        # Used by `nix build` & `nix run`
-        defaultPackage = project false;
+        in
+        {
+          # Used by `nix build` & `nix run`
+          defaultPackage = project false;
 
-        # Used by `nix develop`
-        devShell = project true;
-      });
+          # Used by `nix develop`
+          devShell = project true;
+        }) // {
+      herculesCI.ciSystems = [ "x86_64-linux" ];
+    };
 }
