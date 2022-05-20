7ee0425b07e8aa739503b4235acc15beb88f5c72
Sridhar Ratnakumar <srid@srid.ca>
2022-05-20 11:14:18 -0400

    remove hercules

--- flake.nix
+++ flake.nix
@@ -10,54 +10,50 @@
     tailwind-haskell.url = "github:srid/tailwind-haskell/master";
   };
   outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          name = "ema-template";
-          overlays = [ ];
-          pkgs = nixpkgs.legacyPackages.${system};
-          inherit (pkgs.lib.trivial) pipe flip;
-          inherit (pkgs.lib.lists) optionals;
-          hp = pkgs.haskellPackages;
-          tailwind-haskell = inputs.tailwind-haskell.defaultPackage.${system};
-          shellDeps = with hp; [
-            cabal-fmt
-            cabal-install
-            ghcid
-            haskell-language-server
-            fourmolu
-            pkgs.nixpkgs-fmt
-            tailwind-haskell
-            pkgs.foreman
-            pkgs.treefmt
-          ];
-          project =
-            { returnShellEnv ? false
-            , withHoogle ? false
-            }:
-            hp.developPackage {
-              inherit returnShellEnv withHoogle name;
-              root = ./.;
-              overrides = self: super: with pkgs.haskell.lib; {
-                ema = inputs.ema.defaultPackage.${system};
-                tailwind = tailwind-haskell;
-              };
-              modifier = drv:
-                pkgs.haskell.lib.overrideCabal drv (oa: {
-                  # All the Cabal-specific overrides go here.
-                  # For examples on what is possible, see:
-                  #   https://github.com/NixOS/nixpkgs/blob/master/pkgs/development/haskell-modules/lib/compose.nix
-                  buildTools = (oa.buildTools or [ ]) ++ optionals returnShellEnv shellDeps;
-                });
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        name = "ema-template";
+        pkgs = nixpkgs.legacyPackages.${system};
+        inherit (pkgs.lib.lists) optionals;
+        hp = pkgs.haskellPackages;
+        tailwind-haskell = inputs.tailwind-haskell.defaultPackage.${system};
+        shellDeps = with hp; [
+          cabal-fmt
+          cabal-install
+          ghcid
+          haskell-language-server
+          fourmolu
+          pkgs.nixpkgs-fmt
+          tailwind-haskell
+          pkgs.foreman
+          pkgs.treefmt
+        ];
+        project =
+          { returnShellEnv ? false
+          , withHoogle ? false
+          }:
+          hp.developPackage {
+            inherit returnShellEnv withHoogle name;
+            root = ./.;
+            overrides = self: super: with pkgs.haskell.lib; {
+              ema = inputs.ema.defaultPackage.${system};
+              tailwind = tailwind-haskell;
             };
-        in
-        {
-          # Used by `nix build` & `nix run`
-          defaultPackage = project { };
+            modifier = drv:
+              pkgs.haskell.lib.overrideCabal drv (oa: {
+                # All the Cabal-specific overrides go here.
+                # For examples on what is possible, see:
+                #   https://github.com/NixOS/nixpkgs/blob/master/pkgs/development/haskell-modules/lib/compose.nix
+                buildTools = (oa.buildTools or [ ]) ++ optionals returnShellEnv shellDeps;
+              });
+          };
+      in
+      {
+        # Used by `nix build` & `nix run`
+        defaultPackage = project { };
 
-          # Used by `nix develop`
-          devShell = project { returnShellEnv = true; withHoogle = true; };
-        }) // {
-      herculesCI.ciSystems = [ "x86_64-linux" ];
-    };
+        # Used by `nix develop`
+        devShell = project { returnShellEnv = true; withHoogle = true; };
+      }
+    );
 }
