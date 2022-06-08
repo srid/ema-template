fc9ec110d402c3d85554a1d9b8e93aeab93045f2
Sridhar Ratnakumar <srid@srid.ca>
2022-06-08 12:08:56 -0400

    Update ema, and use haskell-flake for nix

--- flake.nix
+++ flake.nix
@@ -1,59 +1,47 @@
 {
   description = "Ema template app";
   inputs = {
-    ema.url = "github:srid/ema/multisite";
-    nixpkgs.follows = "ema/nixpkgs";
-    flake-utils.follows = "ema/flake-utils";
-    flake-compat.follows = "ema/flake-compat";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs.follows = "nixpkgs";
+    haskell-flake.url = "github:srid/haskell-flake";
 
     # Haskell overrides
+    ema.url = "github:srid/ema/multisite";
+    ema.flake = false;
     tailwind-haskell.url = "github:srid/tailwind-haskell/master";
+    tailwind-haskell.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        name = "ema-template";
-        pkgs = nixpkgs.legacyPackages.${system};
-        inherit (pkgs.lib.lists) optionals;
-        hp = pkgs.haskellPackages;
-        tailwind-haskell = inputs.tailwind-haskell.defaultPackage.${system};
-        shellDeps = with hp; [
-          cabal-fmt
-          cabal-install
-          ghcid
-          haskell-language-server
-          fourmolu
-          pkgs.nixpkgs-fmt
-          tailwind-haskell
-          pkgs.foreman
-          pkgs.treefmt
-        ];
-        project =
-          { returnShellEnv ? false
-          , withHoogle ? false
-          }:
-          hp.developPackage {
-            inherit returnShellEnv withHoogle name;
-            root = ./.;
-            overrides = self: super: with pkgs.haskell.lib; {
-              ema = inputs.ema.defaultPackage.${system};
-              tailwind = tailwind-haskell;
-            };
-            modifier = drv:
-              pkgs.haskell.lib.overrideCabal drv (oa: {
-                # All the Cabal-specific overrides go here.
-                # For examples on what is possible, see:
-                #   https://github.com/NixOS/nixpkgs/blob/master/pkgs/development/haskell-modules/lib/compose.nix
-                buildTools = (oa.buildTools or [ ]) ++ optionals returnShellEnv shellDeps;
-              });
+  outputs = inputs@{ self, nixpkgs, flake-parts, haskell-flake, ... }:
+    flake-parts.lib.mkFlake { inherit self; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      imports = [
+        haskell-flake.flakeModule
+      ];
+      perSystem = { self', inputs', pkgs, ... }: {
+        # "haskellProjects" comes from https://github.com/srid/haskell-flake
+        haskellProjects.default = {
+          root = ./.;
+          buildTools = hp: {
+            inherit (pkgs)
+              treefmt
+              nixpkgs-fmt
+              foreman;
+            inherit (hp)
+              cabal-fmt
+              fourmolu;
+            inherit (inputs'.tailwind-haskell.packages)
+              tailwind;
           };
-      in
-      {
-        # Used by `nix build` & `nix run`
-        defaultPackage = project { };
-
-        # Used by `nix develop`
-        devShell = project { returnShellEnv = true; withHoogle = true; };
-      }
-    );
+          source-overrides = {
+            inherit (inputs)
+              ema;
+          };
+          overrides = self: super: with pkgs.haskell.lib; {
+            inherit (inputs'.tailwind-haskell.packages)
+              tailwind;
+          };
+        };
+      };
+    };
 }
