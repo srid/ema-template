ef299f3cbb0f921d484e7f44dda04b863cffe33c
Sridhar Ratnakumar <srid@srid.ca>
2023-06-08 14:06:10 -0400

    nix: Update all inputs

--- flake.nix
+++ flake.nix
@@ -2,17 +2,16 @@
   description = "Ema template app";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
     flake-parts.url = "github:hercules-ci/flake-parts";
     haskell-flake.url = "github:srid/haskell-flake";
     flake-root.url = "github:srid/flake-root";
     proc-flake.url = "github:srid/proc-flake";
     mission-control.url = "github:Platonic-Systems/mission-control";
-
-    nixpkgs-140774-workaround.url = "github:srid/nixpkgs-140774-workaround";
   };
-  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = nixpkgs.lib.systems.flakeExposed;
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
       imports = [
         inputs.haskell-flake.flakeModule
         inputs.flake-root.flakeModule
@@ -21,10 +20,7 @@
       ];
       perSystem = { self', config, inputs', pkgs, lib, ... }: {
         # "haskellProjects" comes from https://github.com/srid/haskell-flake
-        haskellProjects.main = {
-          imports = [
-            inputs.nixpkgs-140774-workaround.haskellFlakeProjectModules.default
-          ];
+        haskellProjects.default = {
           devShell.tools = hp: {
             inherit (pkgs)
               treefmt
@@ -32,13 +28,16 @@
             inherit (hp)
               cabal-fmt tailwind fourmolu;
           };
+          autoWire = [ "packages" "apps" "checks" ];
         };
 
+        # From https://github.com/srid/proc-flake
         proc.groups.run.processes = {
           haskell.command = "ghcid";
           tailwind.command = "${lib.getExe pkgs.haskellPackages.tailwind} -w -o ./static/tailwind.css './src/**/*.hs'";
         };
 
+        # From https://github.com/Platonic-Systems/mission-control
         mission-control.scripts = {
           docs = {
             description = "Start Hoogle server for project dependencies";
@@ -74,20 +73,26 @@
                 { }
                 ''
                   mkdir -p $out
-                  pushd ${self}
-                  ${lib.getExe config.packages.main-ema-template} \
+                  pushd ${inputs.self}
+                  ${lib.getExe config.packages.ema-template} \
                     --base-url=${baseUrl} gen $out
                   ${lib.getExe pkgs.haskellPackages.tailwind} \
                     -o $out/tailwind.css 'src/**/*.hs'
                 '';
           in
           {
-            default = config.packages.main-ema-template;
+            default = config.packages.ema-template;
             site = buildEmaSiteWithTailwind { baseUrl = "/"; };
             site-github = buildEmaSiteWithTailwind { baseUrl = "/ema-template/"; };
           };
 
-        devShells.default = config.mission-control.installToDevShell config.devShells.main;
+        devShells.default = pkgs.mkShell {
+          name = "ema-template";
+          inputsFrom = [
+            config.mission-control.devShell
+            config.haskellProjects.default.outputs.devShell
+          ];
+        };
       };
     };
 }
