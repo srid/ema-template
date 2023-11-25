32b58635cbbc251a9d9ff634d457f6fb8f003e4c
Sridhar Ratnakumar <srid@srid.ca>
2023-11-25 12:26:51 -0500

    nix: Update flake
    Reintroduce ema for base version relaxation.

--- flake.nix
+++ flake.nix
@@ -10,6 +10,9 @@
     proc-flake.url = "github:srid/proc-flake";
     mission-control.url = "github:Platonic-Systems/mission-control";
     treefmt-nix.url = "github:numtide/treefmt-nix";
+
+    ema.url = "github:srid/ema";
+    ema.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
@@ -21,98 +24,111 @@
         inputs.mission-control.flakeModule
         inputs.treefmt-nix.flakeModule
       ];
-      perSystem = { self', config, inputs', pkgs, lib, ... }: {
-        # "haskellProjects" comes from https://github.com/srid/haskell-flake
-        haskellProjects.default = {
-          devShell.tools = hp: {
-            inherit (hp) tailwind;
+      perSystem = { self', config, inputs', pkgs, lib, ... }:
+        let
+          tailwind = config.haskellProjects.default.outputs.finalPackages.tailwind;
+        in
+        {
+          # "haskellProjects" comes from https://github.com/srid/haskell-flake
+          haskellProjects.default = {
+            imports = [
+              inputs.ema.haskellFlakeProjectModules.output
+            ];
+            settings = {
+              tailwind = {
+                broken = false;
+                jailbreak = true;
+              };
+            };
+            autoWire = [ "packages" "apps" "checks" ];
           };
-          autoWire = [ "packages" "apps" "checks" ];
-        };
 
-        # Auto formatters. This also adds a flake check to ensure that the
-        # source tree was auto formatted.
-        treefmt.config = {
-          inherit (config.flake-root) projectRootFile;
-          package = pkgs.treefmt;
+          # Auto formatters. This also adds a flake check to ensure that the
+          # source tree was auto formatted.
+          treefmt.config = {
+            inherit (config.flake-root) projectRootFile;
+            package = pkgs.treefmt;
 
-          programs.ormolu.enable = true;
-          programs.nixpkgs-fmt.enable = true;
-          programs.cabal-fmt.enable = true;
-          programs.hlint.enable = true;
+            programs.ormolu.enable = true;
+            programs.nixpkgs-fmt.enable = true;
+            programs.cabal-fmt.enable = true;
+            programs.hlint.enable = true;
 
-          # We use fourmolu
-          programs.ormolu.package = pkgs.haskellPackages.fourmolu;
-          settings.formatter.ormolu = {
-            options = [
-              "--ghc-opt"
-              "-XImportQualifiedPost"
+            # We use fourmolu
+            programs.ormolu.package = pkgs.haskellPackages.fourmolu;
+            settings.formatter.ormolu = {
+              options = [
+                "--ghc-opt"
+                "-XImportQualifiedPost"
+              ];
+            };
+          };
+
+          # From https://github.com/Platonic-Systems/process-compose-flake
+          proc.groups.run = {
+            processes = {
+              haskell.command = "ghcid";
+              tailwind.command = "${lib.getExe tailwind} -w -o ./static/tailwind.css './src/**/*.hs'";
+            };
+          };
+
+          # From https://github.com/Platonic-Systems/mission-control
+          mission-control.scripts = {
+            docs = {
+              description = "Start Hoogle server for project dependencies";
+              exec = ''
+                echo http://127.0.0.1:8888
+                hoogle serve -p 8888 --local
+              '';
+            };
+            repl = {
+              description = "Start the cabal repl";
+              exec = ''
+                cabal repl "$@"
+              '';
+            };
+            fmt = {
+              description = "Auto-format the source tree";
+              exec = config.treefmt.build.wrapper;
+            };
+            run = {
+              description = "Run the dev server (ghcid + tailwind)";
+              exec = config.proc.groups.run.package;
+            };
+          };
+
+          packages =
+            let
+              buildEmaSiteWithTailwind = { baseUrl }:
+                pkgs.runCommand "site"
+                  { }
+                  ''
+                    mkdir -p $out
+                    pushd ${inputs.self}
+                    ${lib.getExe config.packages.ema-template} \
+                      --base-url=${baseUrl} gen $out
+                    ${lib.getExe tailwind} \
+                      -o $out/tailwind.css 'src/**/*.hs'
+                  '';
+            in
+            {
+              default = config.packages.ema-template;
+              site = buildEmaSiteWithTailwind { baseUrl = "/"; };
+              site-github = buildEmaSiteWithTailwind { baseUrl = "/ema-template/"; };
+            };
+
+          devShells.default = pkgs.mkShell {
+            name = "ema-template";
+            packages = [
+              tailwind
+            ];
+            inputsFrom = [
+              config.haskellProjects.default.outputs.devShell
+              config.treefmt.build.devShell
+              config.mission-control.devShell
+              config.flake-root.devShell
             ];
           };
         };
-
-        # From https://github.com/Platonic-Systems/process-compose-flake
-        proc.groups.run = {
-          processes = {
-            haskell.command = "ghcid";
-            tailwind.command = "${lib.getExe pkgs.haskellPackages.tailwind} -w -o ./static/tailwind.css './src/**/*.hs'";
-          };
-        };
-
-        # From https://github.com/Platonic-Systems/mission-control
-        mission-control.scripts = {
-          docs = {
-            description = "Start Hoogle server for project dependencies";
-            exec = ''
-              echo http://127.0.0.1:8888
-              hoogle serve -p 8888 --local
-            '';
-          };
-          repl = {
-            description = "Start the cabal repl";
-            exec = ''
-              cabal repl "$@"
-            '';
-          };
-          fmt = {
-            description = "Auto-format the source tree";
-            exec = config.treefmt.build.wrapper;
-          };
-          run = {
-            description = "Run the dev server (ghcid + tailwind)";
-            exec = config.proc.groups.run.package;
-          };
-        };
-
-        packages =
-          let
-            buildEmaSiteWithTailwind = { baseUrl }:
-              pkgs.runCommand "site"
-                { }
-                ''
-                  mkdir -p $out
-                  pushd ${inputs.self}
-                  ${lib.getExe config.packages.ema-template} \
-                    --base-url=${baseUrl} gen $out
-                  ${lib.getExe pkgs.haskellPackages.tailwind} \
-                    -o $out/tailwind.css 'src/**/*.hs'
-                '';
-          in
-          {
-            default = config.packages.ema-template;
-            site = buildEmaSiteWithTailwind { baseUrl = "/"; };
-            site-github = buildEmaSiteWithTailwind { baseUrl = "/ema-template/"; };
-          };
-
-        devShells.default = pkgs.mkShell {
-          name = "ema-template";
-          inputsFrom = [
-            config.haskellProjects.default.outputs.devShell
-            config.treefmt.build.devShell
-            config.mission-control.devShell
-            config.flake-root.devShell
-          ];
-        };
-      };
     };
 }
